293179fe702164e24145780194899e9b69a5a87b
mmzk1526 <yc4120@ic.ac.uk>
2023-12-23 21:02:49 +0000

    [mmzk] (feat) Add Bifunctor & String to MMZK.Function

--- src/MMZK/Function.hs
+++ src/MMZK/Function.hs
@@ -2,6 +2,7 @@
 module MMZK.Function
   ( module Control.Applicative
   , module Control.Monad
+  , module Data.Bifunctor
   , module Data.Bool
   , module Data.Char
   , module Data.Function
@@ -11,12 +12,14 @@ module MMZK.Function
   , module Data.List
   , module Data.Monoid
   , module Data.Ord
+  , module Data.String
   , module Data.Traversable
   , module Data.Tuple
   ) where
 
 import           Control.Applicative
 import           Control.Monad
+import           Data.Bifunctor
 import           Data.Bool
 import           Data.Char
 import           Data.Function
@@ -26,5 +29,6 @@ import           Data.Foldable
 import           Data.List
 import           Data.Monoid
 import           Data.Ord
+import           Data.String
 import           Data.Traversable
 import           Data.Tuple
