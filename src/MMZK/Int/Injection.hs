0fe263241f422a80e5bdb121c186266fcba2d7eb
mmzk1526 <yc4120@ic.ac.uk>
2024-02-28 16:22:51 +0000

    Conditional tests for Int.Injection based on WORD_SIZE_IN_BITS

--- src/MMZK/Int/Injection.hs
+++ src/MMZK/Int/Injection.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE UndecidableInstances #-}
 
 #include "MachDeps.h"
 
--- test/Int/Injection/Spec.hs
+++ test/Int/Injection/Spec.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE CPP #-}
+
+#include "MachDeps.h"
+
 module Int.Injection.Spec where
 
 import           MMZK.Int.Injection
@@ -9,25 +13,41 @@ intInjectionSpec = describe "MMZK.Int.Injection" do
   describe "Intable for Int types" do
     it "Int" do
       property $ roundtripP @Int
+#if WORD_SIZE_IN_BITS >= 8
     it "Int8" do
       property $ roundtripP @Int8
+#endif
+#if WORD_SIZE_IN_BITS >= 16
     it "Int16" do
       property $ roundtripP @Int16
+#endif
+#if WORD_SIZE_IN_BITS >= 32
     it "Int32" do
       property $ roundtripP @Int32
+#endif
+#if WORD_SIZE_IN_BITS >= 64
     it "Int64" do
       property $ roundtripP @Int64
+#endif
   describe "Intable for Word types" do
     it "Word" do
       property $ roundtripP @Word
+#if WORD_SIZE_IN_BITS >= 8
     it "Word8" do
       property $ roundtripP @Word8
+#endif
+#if WORD_SIZE_IN_BITS >= 16
     it "Word16" do
       property $ roundtripP @Word16
+#endif
+#if WORD_SIZE_IN_BITS >= 32
     it "Word32" do
       property $ roundtripP @Word32
+#endif
+#if WORD_SIZE_IN_BITS >= 64
     it "Word64" do
       property $ roundtripP @Word64
+#endif
   describe "Intable for other types" do
     it "()" do
       convertToInt () `shouldBe` 0
