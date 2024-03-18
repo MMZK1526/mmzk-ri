60aa456cec4f37a5a645b85f3826bcc5263dc09a
mmzk1526 <yc4120@ic.ac.uk>
2024-03-18 19:30:23 +0000

    Use TypeNat in Fixed List

--- src/MMZK/List/Fixed/Internal.hs
+++ src/MMZK/List/Fixed/Internal.hs
@@ -1,35 +1,66 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module MMZK.List.Fixed.Internal where
 
 import           Data.Foldable
+import           Data.Proxy
+import           Data.Type.Ord
+import           GHC.TypeLits
 
-data Peano = Z | S Peano
+type family IsZero (n :: Nat) :: Bool where
+  IsZero 0 = 'True
+  IsZero _ = 'False
 
-type family Plus (n :: Peano) (m :: Peano) :: Peano where
-  Plus Z     m = m
-  Plus (S n) m = S (Plus n m)
+class Eq' a (b :: Bool) where
+  eq' :: Proxy b -> a -> a -> Bool
 
-type family Minus (n :: Peano) (m :: Peano) :: Peano where
-  Minus n     Z     = n
-  Minus (S n) (S m) = Minus n m
+class Ord' a (b :: Bool) where
+  compare' :: Proxy b -> a -> a -> Ordering
 
-data ListFixed (len :: Peano) e where
-  Nil  :: ListFixed Z e
-  (:~) :: e -> ListFixed n e -> ListFixed (S n) e
+infixr 5 :~
+data ListFixed (len :: Nat) e where
+  Nil  :: ListFixed 0 e
+  (:~) :: n > 0 => e -> ListFixed (n - 1) e -> ListFixed n e
 
-instance Eq e => Eq (ListFixed len e) where
-  (==) :: ListFixed len e -> ListFixed len e -> Bool
-  Nil == Nil             = True
-  (x :~ xs) == (y :~ ys) = x == y && xs == ys
-  {-# INLINE (==) #-}
+instance (Eq e, Eq' (ListFixed (len :: Nat) e) (IsZero len))
+  => Eq (ListFixed len e) where
+    (==) :: ListFixed len e -> ListFixed len e -> Bool
+    (==) = eq' (Proxy :: Proxy (IsZero len))
+    {-# INLINE (==) #-}
 
-instance Ord e => Ord (ListFixed len e) where
-  compare :: ListFixed len e -> ListFixed len e -> Ordering
-  compare Nil Nil             = EQ
-  compare (x :~ xs) (y :~ ys) = compare x y <> compare xs ys
-  {-# INLINE compare #-}
+instance ( Ord e, Eq (ListFixed (len :: Nat) e)
+         , Ord' (ListFixed (len :: Nat) e) (IsZero len)
+         ) =>
+  Ord (ListFixed len e) where
+    compare :: ListFixed len e -> ListFixed len e -> Ordering
+    compare = compare' (Proxy :: Proxy (IsZero len))
+    {-# INLINE compare #-}
+
+instance Eq' (ListFixed 0 e) 'True where
+  eq' :: Proxy 'True -> ListFixed 0 e -> ListFixed 0 e -> Bool
+  eq' _ _ _ = True
+  {-# INLINE eq' #-}
+
+instance (Eq e, Eq' (ListFixed (n - 1) e) (IsZero (n - 1)), n > 0)
+  => Eq' (ListFixed n e) 'False where
+    eq' :: Proxy 'False -> ListFixed n e -> ListFixed n e -> Bool
+    eq' _ (x :~ xs) (y :~ ys) = x == y && eq' (Proxy @(IsZero (n - 1))) xs ys
+    {-# INLINE eq' #-}
+
+instance Ord' (ListFixed 0 e) 'True where
+  compare' :: Proxy 'True -> ListFixed 0 e -> ListFixed 0 e -> Ordering
+  compare' _ _ _ = EQ
+  {-# INLINE compare' #-}
+
+instance ( Ord e, Ord (ListFixed (n - 1) e)
+         , Ord' (ListFixed (n - 1) e) (IsZero (n - 1)), n > 0
+         ) => Ord' (ListFixed n e) 'False where
+    compare' :: Proxy 'False -> ListFixed n e -> ListFixed n e -> Ordering
+    compare' _ (x :~ xs) (y :~ ys)
+      = compare x y <> compare' (Proxy @(IsZero (n - 1))) xs ys
+    {-# INLINE compare' #-}
 
 instance Show e => Show (ListFixed len e) where
   show :: ListFixed len e -> String
@@ -60,34 +91,34 @@ instance Traversable (ListFixed len) where
   traverse f (x :~ xs) = (:~) <$> f x <*> traverse f xs
   {-# INLINE traverse #-}
 
-head :: ListFixed (S n) e -> e
+head :: n > 0 => ListFixed n e -> e
 head (x :~ _) = x
 {-# INLINE head #-}
 
-tail :: ListFixed (S n) e -> ListFixed n e
+tail :: n > 0 => ListFixed n e -> ListFixed (n - 1) e
 tail (_ :~ xs) = xs
 {-# INLINE tail #-}
 
-init :: ListFixed (S n) e -> ListFixed n e
-init (_ :~ Nil)        = Nil
-init (x :~ (x' :~ xs)) = x :~ MMZK.List.Fixed.Internal.init (x' :~ xs)
+init :: n > 0 => ListFixed n e -> ListFixed (n - 1) e
+init (_ :~ Nil)          = Nil
+init (x :~ xs'@(_ :~ _)) = x :~ MMZK.List.Fixed.Internal.init xs'
 {-# INLINE init #-}
 
-last :: ListFixed (S n) e -> e
+last :: n > 0 => ListFixed n e -> e
 last (x :~ Nil)        = x
 last (_ :~ (x' :~ xs)) = MMZK.List.Fixed.Internal.last (x' :~ xs)
 {-# INLINE last #-}
 
-(++) :: ListFixed n e -> ListFixed m e -> ListFixed (Plus n m) e
-Nil ++ ys       = ys
-(x :~ xs) ++ ys = x :~ (xs MMZK.List.Fixed.Internal.++ ys)
-{-# INLINE (++) #-}
+-- (++) :: ListFixed n e -> ListFixed m e -> ListFixed (Plus n m) e
+-- Nil ++ ys       = ys
+-- (x :~ xs) ++ ys = x :~ (xs MMZK.List.Fixed.Internal.++ ys)
+-- {-# INLINE (++) #-}
 
 null :: ListFixed len e -> Bool
 null Nil = True
 null _   = False
 {-# INLINE null #-}
 
-singleton :: e -> ListFixed (S Z) e
+singleton :: e -> ListFixed 1 e
 singleton x = x :~ Nil
 {-# INLINE singleton #-}
